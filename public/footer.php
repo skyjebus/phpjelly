75752cf5f1847a41589ae7d51e1d4d1552689cf4
SkyJebus <64335706+skyjebus@users.noreply.github.com>
2022-02-08 15:18:40 -0500

    Cache test

--- public/footer.php
+++ public/footer.php
@@ -88,3 +88,11 @@
 </body>
 
 </html>
+
+<?php
+// Cache the contents to a cache file
+$cached = fopen($cachefile, 'w');
+fwrite($cached, ob_get_contents());
+fclose($cached);
+ob_end_flush(); // Send the output to the browser
+?>
