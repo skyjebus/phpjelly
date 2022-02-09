4582c4ead8ffffff233e7d0a06d6ba90c922a231
SkyJebus <64335706+skyjebus@users.noreply.github.com>
2022-02-09 13:50:32 -0500

    Prettier

--- public/script-header.php
+++ public/script-header.php
@@ -1,20 +1,60 @@
 <?php
+/**
+ * @format
+ */
+
 // website config
-$name = 'Company Name';
-$address = '1313 Mockingbird Ln.';
-$al2 = '#1'; // address line two
-$city = 'Boston';
-$state = 'MA';
-$zip = '12345';
-$phone = '555-867-5309';
-$hours = 'M-F 9:00am - 5:00pm';
+$name = "Company Name";
+$address = "1313 Mockingbird Ln.";
+$al2 = "#1"; // address line two
+$city = "Boston";
+$state = "MA";
+$zip = "12345";
+$phone = "555-867-5309";
+$hours = "M-F 9:00am - 5:00pm";
 
 // socail media links config
-$gmb = 'test1';
-$fb = 'test2';
-$ig = 'test3';
-$twt = 'test4';
-$yelp = 'test5';
+$gmb = "test1";
+$fb = "test2";
+$ig = "test3";
+$twt = "test4";
+$yelp = "test5";
+
+// responsive image via php function
+function image1()
+{
+  $imgName = "fv";
+  $imgLocation = $GLOBALS['$imgLocation'];
+  echo '<img class="w-32 h-48 object-cover object-center rounded-lg mx-auto lg:mx-0 bg-gray-200 dark:bg-gray-900 backdrop-blur-3xl transition motion-reduce:transition-none duration-250"' .
+    'data-src="' .
+    $imgLocation .
+    $imgName .
+    '-xs.jpg"' .
+    'srcset="' .
+    $imgLocation .
+    $imgName .
+    "-xs.jpg 320w," .
+    $imgLocation .
+    $imgName .
+    "-sm.jpg 375w," .
+    $imgLocation .
+    $imgName .
+    "-md.jpg 768w," .
+    $imgLocation .
+    $imgName .
+    "-lg.jpg 960w," .
+    $imgLocation .
+    $imgName .
+    "-lg.jpg 1024w," .
+    $imgLocation .
+    $imgName .
+    "-xl.jpg 1400w," .
+    $imgLocation .
+    $imgName .
+    '-2xl.jpg 1536w,"' .
+    'sizes="50vmin, 100vmax" width="1" height="1"' .
+    'loading="eager" alt="Descriptive alt text here!">';
+}
 
 ############################################################################################
 ############## DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING ###############
@@ -22,32 +62,18 @@ $yelp = 'test5';
 
 // hostname
 $domain = siteURL();
-function siteURL() {
-$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || 
-$_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-$domainName = $_SERVER['HTTP_HOST'];
-return $protocol.$domainName;
+function siteURL()
+{
+  $protocol = (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] != "off") || $_SERVER["SERVER_PORT"] == 443 ? "https://" : "http://";
+  $domainName = $_SERVER["HTTP_HOST"];
+  return $protocol . $domainName;
 }
 
 // canonical Links
-$canonicalFilename = 'https://' . $_SERVER['HTTP_HOST'] . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], ""));
+$canonicalFilename = "https://" . $_SERVER["HTTP_HOST"] . substr($_SERVER["REQUEST_URI"], 0, strrpos($_SERVER["REQUEST_URI"], ""));
 
 // globals image path
-$GLOBALS['$imgLocation'] = '../images/';
-
-$url = $_SERVER["SCRIPT_NAME"];
-$break = Explode('/', $url);
-$file = $break[count($break) - 1];
-$cachefile = 'cached-'.substr_replace($file ,"",-4).'.html';
-$cachetime = 18000;
-
-// Serve from the cache if it is younger than $cachetime
-if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
-    echo "<!-- Cached copy, generated ".date('H:i', filemtime($cachefile))." -->\n";
-    readfile($cachefile);
-    exit;
-}
-ob_start(); // Start the output buffer
+$GLOBALS['$imgLocation'] = "../images/";
 ?>
 
 <!-- script header -->
@@ -55,23 +81,28 @@ ob_start(); // Start the output buffer
 
 <!-- empty html class for dark mode -->
 <html lang="en" class="">
+
 <head>
-<link rel="canonical" href="<?php echo $canonicalFilename; ?>">
-<script>
-if (localStorage.theme === 'system' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  html.classList.add("dark") // inline to prevent FOUC
-}
-</script>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="canonical" href="<?php echo $canonicalFilename; ?>">
+  <script>
+  if (localStorage.theme === 'system' || (!('theme' in localStorage) && window.matchMedia(
+        '(prefers-color-scheme: dark)')
+      .matches)) {
+    html.classList.add("dark") // inline to prevent FOUC
+  }
+  </script>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- load javascript first -->
-<script src="/javascript/main.js" defer></script>
-<script src="/javascript/cookieconsent.js" defer></script>
+  <!-- load javascript first -->
+  <script src="/javascript/main.js" defer></script>
+  <script src="/javascript/cookieconsent.js" defer></script>
 
-<!-- inline css for second -->
-<style><?php require $docRoot.'/css/styles.css'; ?></style>
+  <!-- inline css for second -->
+  <style>
+  <?php require $docRoot . "/css/styles.css"; ?>
+  </style>
 
-<!-- fav icon -->
-<?php require $docRoot.'/favicon/favicons-meta-tags.html'; ?>
+  <!-- fav icon -->
+  <?php require $docRoot . "/favicon/favicons-meta-tags.html"; ?>
--- public/watcher.php
+++ public/watcher.php
@@ -1,47 +1,52 @@
 <?php
+/**
+ * @format
+ */
 
 error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-date_default_timezone_set("UTC"); 
-header('Content-Type: application/json');
-session_start();  //used to keep track of when the file changes
-$changed=false; //assume tha the file has not changed
+ini_set("display_errors", "On");
+date_default_timezone_set("UTC");
+header("Content-Type: application/json");
+session_start(); //used to keep track of when the file changes
+$changed = false; //assume tha the file has not changed
 
-$basename= basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']);
+$basename = basename($_SERVER["REQUEST_URI"], "?" . $_SERVER["QUERY_STRING"]);
 
-$referrer=isset($_SERVER['HTTP_REFERER'])? $_SERVER['HTTP_REFERER'] : './css/styles.css' ;
+$referrer = isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : "./css/styles.css";
 
-$filename = isset($_REQUEST['file'])? trim($_REQUEST['file'])  :$referrer ; //assume that the calling web-
+$filename = isset($_REQUEST["file"]) ? trim($_REQUEST["file"]) : $referrer; //assume that the calling web-
 
-if (@file_exists($filename))
-   // deepcode ignore InsecureHash: <please specify a reason of ignoring this>
-   $sha1_file=sha1_file($filename);	//let's get the sha1 file info
-else
-die(json_encode("file [$filename] MISSING, filename does not exist. Check name and path ."));	
+if (@file_exists($filename)) {
+  // deepcode ignore InsecureHash: <please specify a reason of ignoring this>
+  $sha1_file = sha1_file($filename);
+}
+//let's get the sha1 file info
+else {
+  die(json_encode("file [$filename] MISSING, filename does not exist. Check name and path ."));
+}
 
 //now let's save the current file
- if  ( isset($_SESSION[$filename] )    ) //do we have this session set?
- {
-    if ( $sha1_file != $_SESSION[$filename] )
-     $changed=true;
-      $_SESSION[$filename] =$sha1_file;  //update the session variable
- }
- else
- {
-  $_SESSION[$filename] =$sha1_file;
- }
+if (isset($_SESSION[$filename])) {
+  //do we have this session set?
+  if ($sha1_file != $_SESSION[$filename]) {
+    $changed = true;
+  }
+  $_SESSION[$filename] = $sha1_file; //update the session variable
+} else {
+  $_SESSION[$filename] = $sha1_file;
+}
 
 //store this information into the file array
 
+$file_data = [
+  "filename" => $filename,
+  "hasChanged" => $changed,
+  "filetime" => date("H:i:s m-d-Y", filemtime($filename)),
+  "filesize" => filesize($filename),
+  "basename" => $basename,
+];
 
-$file_data= array("filename" =>$filename, 
- 	 "hasChanged" => $changed,
-	  "filetime" => date ("H:i:s m-d-Y", filemtime($filename)),
-	  "filesize" => filesize($filename),
-	  "basename" => $basename
- 	  );
-
-$result= json_encode($file_data);
+$result = json_encode($file_data);
 
 echo $result;
 
